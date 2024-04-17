5b63e216aed70ddb74a50bf6adb3a63c8421d68c
Puvvada venkata siva satya sai kumar <psaikumar98154@gmail.com>
2024-04-17 21:46:50 +0530

    first commit

---
+++ text-color/src/color-change.js
@@ -0,0 +1,11 @@
+import React , { Component } from "react";
+class ColorChanger extends Component{
+constructor(props){
+    super(props);
+    this.state ={
+        textColor : "black";
+        fontSize : "18px";
+        fontStyle : "italic";
+    };
+}
+}
